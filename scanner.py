edf1ee8c62e80a8d476a20529eb32be3df7df9ce
Jordan R <github@cjrash.com>
2016-12-02 15:32:26 -0500

    Pre-programmed scans added

--- scanner.py
+++ scanner.py
@@ -11,7 +11,11 @@ import shutil
 
 class Scanner():
 	
-	def __init__(self, flags = '-n -Pn -sV -O', network = '127.0.0.1', ports = '1-1024', infile = None, wtd = None, async = None):
+	def __init__(self, flags = '', network = '127.0.0.1', ports = '1-1024', infile = None, wtd = None, async = None, preone = None, pretwo = None, prethree = None, prefour = None):
+		self.inPorts = ports
+		self.inNetwork = network
+		self.writeFile = wtd
+		self.scanResults = dict()
 		if async:
 			self.nm = nmap.PortScannerAsync()
 			self.completed = 0
@@ -20,14 +24,28 @@ class Scanner():
 		else:
 			self.nm = nmap.PortScanner()
 		self.async = async
-		self.flags = flags
-		self.inNetwork = network
-		self.inPorts = ports
-		self.writeFile = wtd
-		self.scanResults = dict()
+		
+		if preone:
+			self.flags = '-sP'
+			self.inPorts = None
+			self.printOption = 1
+		elif pretwo:
+			self.flags = '--open -sV'
+			self.inPorts = pretwo
+			self.printOption = 2
+		elif prethree:
+			self.printOption = 3
+			self.flags = '-n -Pn -sV -O -T4 --reason'
+		elif prefour:
+			slef.printOption = 4
+			self.flags = ''
+		else:
+			self.printOption = 0
+			self.flags = flags
+		
 		if infile:
 			try: 
-				self.scanResults = pickle.load(open("scanResults.p", "rb"))
+				self.scanResults = pickle.load(open("./.scanResults.p", "rb"))
 			except IOError:
 				print("No file exists, must run a scan first")
 				sys.exit()
@@ -51,8 +69,6 @@ class Scanner():
 			self.scanResults = self.nm.scan(hosts=self.inNetwork, ports=self.inPorts, arguments=self.flags, callback=self.statusMonitor)
 			sys.stdout.write("[%s] 0%%\r" % (" " * 50))
 			sys.stdout.flush()
-			#sys.stdout.write("\b" * 51)
-			#sys.stdout.flush()
 			while self.nm.still_scanning():
 				continue
 		else:
@@ -63,7 +79,8 @@ class Scanner():
 		except (IOError, OSError, AttributeError):
 			pass
 		
-		pickle.dump(self.scanResults, open("scanResults.p", "wb"))
+		pickle.dump(self.scanResults, open("./.scanResults.p", "wb"))
+		self.printResults()
 	
 	def statusMonitor(self, host, results):
 		self.completed += 1
@@ -71,12 +88,12 @@ class Scanner():
 		self.percent = int((self.completed / float(256)) * 100)
 		if self.percent % 2 == 0 and opercent != self.percent:
 			self.ticks += 1
-		sys.stdout.write("[%s%s] %s%%\r" % (("-" * self.ticks), " " * (50 - self.ticks), self.percent))
+		sys.stdout.write("[%s%s] %s%%\r" % (("=" * self.ticks), " " * (50 - self.ticks), self.percent))
 		sys.stdout.flush()	
 	
 	def sortByPorts(self):
 		try:
-			shutil.rmtree('./nmap_results') # THIS NEEDS TO BE CLEANER
+			shutil.rmtree('./nmap_results/sortByPort') # THIS NEEDS TO BE CLEANER
 		except OSError:
 			pass
 
@@ -91,28 +108,59 @@ class Scanner():
 				pass
 
 	def printResults(self):
-		pprint.pprint(self.scanResults)
-
+		#self.sortByPorts()
+		if self.printOption == 0:
+			pprint.pprint(self.scanResults)
+		elif self.printOption == 1:
+			for i in self.scanResults["scan"]:
+				print("%s: %s" % (self.scanResults["scan"][i]["addresses"]["ipv4"], self.scanResults["scan"][i]["status"]["state"]))
+		elif self.printOption == 2:
+				for i in self.scanResults['scan']:
+					try:
+						print("%s:%s - %s\t%s %s" % (self.scanResults['scan'][i]['addresses']['ipv4'], self.inPorts, self.scanResults['scan'][i]['status']['state'], self.scanResults['scan'][i]['tcp'][int(self.inPorts)]['product'],self.scanResults['scan'][i]['tcp'][int(self.inPorts)]['extrainfo']))
+					except KeyError:
+						pass
+		elif self.printOption == 3:
+				pass
+		elif self.printOption == 4:
+				pass
 
 if __name__ == "__main__":
 	p = argparse.ArgumentParser(description='Python NMAP Wrapper', formatter_class=argparse.RawTextHelpFormatter)
+
+	g1 = p.add_argument_group("Pre-programmed NMAP Scans", "Suggested NMAP scans to accomplish specific tasks")
+	g1.add_argument('-1', help="Basic ping sweep scan. Host that appear up print to screen.\nOPTIONS = -sP", dest="preone",  action="store_true")
+	g1.add_argument('-2', help="Returns machines with port PORTNUM open.", dest="pretwo", metavar='PORTNUM')
+	g1.add_argument('-3', help="Aggressive scan.  Will take a very long time.\nOPTIONS = -n -Pn -sV -O -T4 --reason", dest="prethree", action="store_true")
+	g1.add_argument('-4', help="Place holder for scan type 4 with NSE support.", dest="prefour", action="store_true")
+
 	p.add_argument('-n', '--network', help='Set the network to scan. Can include CIDR.\nDEFAULT = 127.0.0.1')
 	p.add_argument('-p', '--ports', help='Set the ports or port range for scan.\nDEFAULT = 1-1024')
-	p.add_argument('-f', '--flags', help="Set NMAP flags for scan. Space delimited.\nDEFAULT = -n -Pn -sV -O")
+	p.add_argument('-f', '--flags', help="Set NMAP flags for scan, space delimited. **Will override mode.**\nDEFAULT = -n -Pn -sV -O")
 	p.add_argument('-w', '--wtd', help='Write output to disk (save .nmap and .gnmap to working directory.\nDEFAULT=False', metavar='FILENAME')
-	p.add_argument('--infile', help="Used for debugging. Uses Python pickle file from previous scan to save time.\nDEFAULT = Looks for file ./scanResults.p", action="store_true") 
+	p.add_argument('--infile', help="Used for debugging. Uses Python pickle file from previous scan to save time.\nDEFAULT = Looks for file ./.scanResults.p", action="store_true") 
 	p.add_argument('--async', help="Used for running hosts asynchronusly.  Can get status updates with this option.\nDEFAULT = No", action="store_true") 
 	flags = vars(p.parse_args())
 	userIn = ""
+	
+	temp = p.parse_args()
+	if temp.preone is True and (temp.pretwo is True or temp.prethree is True or temp.prefour is True):
+		print("Error: Only one pre-programmed scan type allowed at once.")
+		sys.exit()
+	elif temp.pretwo is True and (temp.prethree is True or temp.prefour is True):
+		print("Error: Only one pro-programmed scan type allowed at once.")
+		sys.exit()
+	elif temp.prethree is True and temp.prefour is True:
+		print("Error: Only one pro-programmed scan type allowed at once.")
+		sys.exit()
 
 	for x in flags:
 		if flags[x]:
 			userIn += ('%s="%s",' % (x,flags[x]))
 	scanner = eval("Scanner(%s)" % userIn[:-1])
+	
+
 	if p.parse_args().infile:
-		scanner.sortByPorts()
 		scanner.printResults()
 	else:
 		scanner.runScan()
-		#scanner.sortByPorts()
-		#scanner.printResults()
